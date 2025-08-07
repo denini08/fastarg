4d75aa4682719ef447d95c38343d004ad732a510
Denini Gabriel <denini.gabriel@gmail.com>
2025-08-07 18:20:56 -0300

    fix: run test on windows

--- tests/test_fastarg.py
+++ tests/test_fastarg.py
@@ -1,5 +1,11 @@
 from src import fastarg
 import subprocess
+import sys
+
+def run_command(command):
+    """Helper function to run a command and return normalized output"""
+    completed_process = subprocess.run(f"{sys.executable} {command}", shell=True, capture_output=True)
+    return completed_process.stdout.decode("utf-8").replace('\r\n', '\n')
 
 def test_foo():
     assert 'foo'.upper() == 'FOO'
@@ -28,17 +34,17 @@ def test_command_get_name():
     assert app.commands[0].get_name() == "foo"
 
 def test_hello_world():
-    completed_process = subprocess.run("python3 main.py hello_world foo", shell=True, capture_output=True)
-    assert completed_process.stdout.decode("utf-8") == "hello foo\n"
+    output = run_command("main.py hello_world foo")
+    assert output == "hello foo\n"
 
 def test_create_todo():
-    completed_process = subprocess.run("python3 main.py todo create_todo \"drink water\"", shell=True, capture_output=True)
-    assert completed_process.stdout.decode("utf-8") == "create todo: drink water - False\n"
+    output = run_command("main.py todo create_todo \"drink water\"")
+    assert output == "create todo: drink water - False\n"
 
 def test_create_todo_completed():
-    completed_process = subprocess.run("python3 main.py todo create_todo \"drink water\" --completed", shell=True, capture_output=True)
-    assert completed_process.stdout.decode("utf-8") == "create todo: drink water - True\n"
+    output = run_command("main.py todo create_todo \"drink water\" --completed")
+    assert output == "create todo: drink water - True\n"
 
 def test_create_address():
-    completed_process = subprocess.run("python3 main.py user address create_address 123 \"456 main st\" --city bellevue --state wa --zip 98004", shell=True, capture_output=True)
-    assert completed_process.stdout.decode("utf-8") == "creating address for user 123\n456 main st bellevue wa 98004\n"
+    output = run_command("main.py user address create_address 123 \"456 main st\" --city bellevue --state wa --zip 98004")
+    assert output == "creating address for user 123\n456 main st bellevue wa 98004\n"
